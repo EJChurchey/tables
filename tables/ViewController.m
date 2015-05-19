1006bc242ac49e1bca10dc432a8c06e8d4ab4ddd
Your Name <everettchurchey@crbc.edu>
2015-05-19 10:10:10 -0400

    tables

--- tables/ViewController.m
+++ tables/ViewController.m
@@ -12,16 +12,31 @@
 
 @end
 
+NSString* CELL_NAME = @"Cell";
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+-(UITableViewCell*) tableView:(UITableView *) tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier: CELL_NAME];
+    //if no reusable cell exists create a new one
+    //nil mean empty
+    
+    if (cell == nil){
+cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier: CELL_NAME];
+        
+}
+    [[cell textLabel] setText: @"Hello World"];
+    return cell;
 }
 
 @end
